02f13a93c1b1b63734023199f48feec349260b74
Polzark <119024334+Polzark@users.noreply.github.com>
2025-05-27 00:48:11 +1000

    Piece Types
    started the code, made classes for every piece type

---
+++ constants.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+#Board Dimensions
+ROWS = 8
+COLS = 1
+
+#Piece types
+class PieceType(Enum):
+    EMPTY = 0
+    PAWN = 1
+    KNIGHT = 2
+    BISHOP = 3
+    ROOK = 4
+    QUEEN = 5
+    KING = 6
+
+class PieceColour(Enum):
+    WHITE = 0
+    BLACK = 1
---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+from constants import * 
+
+# Create start positions
+Start = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+Prev = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+print(Start)
+
+# wait for all pieces to be placed
+
+
+# Start game
