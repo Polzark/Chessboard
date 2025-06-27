a7e84e519f733f7429149607f42385216ee4d8f5
Tet Nay Lin <tetnaylin168@gmail.com>
2025-06-27 23:06:34 +1000

    Add binary and square 2D arrays

--- mux_reader_v_1_0.py
+++ mux_reader_v_1_0.py
@@ -1,6 +1,7 @@
 import time
 import datetime
 import RPi.GPIO as GPIO
+import chess
 
 ###############################################################################
 ######################### --- Documentation --- ###############################
@@ -30,11 +31,27 @@ s1 = [0,0,1,1,0,0,1,1] # s1 values
 s2 = [0,0,0,0,1,1,1,1] # s2 values
 signal_reading = [0,0,0,0,0,0,0,0] # Initialise signal_reading
 
+board = chess.Board()
+
+# Cali's part: Creating a 8x8 array for the chessboard, all initialised to 0.
+# Might changed later after Cali finishes her implementation.
+rows, cols = (8, 8)
+chessboard_binary = [[0 for i in range(cols)] for j in range(rows)]
+
+# Rows are reversed to start from bottom left: A1
+chessboard_square = [[chess.square(file, 7 - rank)
+                      for file in range(cols)] for rank in range(rows)]
+
 ###############################################################################
 
 ###############################################################################
 ###################### --- All helper functions --- ###########################
 ###############################################################################
+
+# Can be integrated into the current_readings loop but separated for now.
+def put_sig_reading_in_chessboard():
+    for row in range(8):
+        chessboard_binary[row][0] = signal_reading[row]
 
 def current_readings():
     timestamp = time.time()
@@ -69,11 +86,12 @@ def main():
     try:
         while True:
             current_readings()
+            put_sig_reading_in_chessboard()
             time.sleep(0.1)
     except KeyboardInterrupt:
         GPIO.cleanup()
 
 if __name__=="__main__":
 	main()
-
+    
 ###############################################################################
