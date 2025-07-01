de339ff21908c0df715dcb153d788076c89ac668
Tet Nay Lin <tetnaylin168@gmail.com>
2025-07-01 21:49:00 +1000

    Add additional documentation and possibly useful initialisations

--- that_chessculated_quickly.py
+++ that_chessculated_quickly.py
@@ -36,25 +36,27 @@ signal_reading = [0,0,0,0,0,0,0,0] # Initialise signal_reading
 
 board = chess.Board()
 
+rows, cols = (8, 8)
 # Cali's part: Creating a 8x8 array for the chessboard, all initialised to 0.
 # Might changed later after Cali finishes her implementation.
-rows, cols = (8, 8)
-chessboard_binary = [[0 for i in range(cols)] for j in range(rows)]
+chessboard_binary_curr = [[0 for i in range(cols)] for j in range(rows)]
+
+# Last reading cycle's chessboard data.
+chessboard_binary_prev = [[0 for i in range(cols)] for j in range(rows)]
 
 # Rows are reversed to start from bottom left: A1
 chessboard_square = [[chess.square(file, 7 - rank)
                       for file in range(cols)] for rank in range(rows)]
+
+# Might use this to light up particular squares on the chessboard.
+# 0 for unlit squares, 1 for lit squares.
+chessboard_light_up = [[0 for i in range(cols)] for j in range(rows)]
 
 ###############################################################################
 
 ###############################################################################
 ###################### --- All helper functions --- ###########################
 ###############################################################################
-
-# Can be integrated into the current_readings loop but separated for now.
-def put_sig_reading_in_chessboard():
-    for row in range(8):
-        chessboard_binary[row][0] = signal_reading[row]
 
 def current_readings():
     timestamp = time.time()
@@ -78,6 +80,12 @@ def current_readings():
         print(i," = ", s0[i],s1[i],s2[i],"Reading: ",signal_reading[i])
 
     print("-----------------------")
+
+# Can be integrated into the current_readings loop but separated for now.
+# Currently, accounts for only the first column of the chessboard lol.
+def put_sig_reading_in_chessboard():
+    for row in range(8):
+        chessboard_binary_curr[row][0] = signal_reading[row]
 
 ###############################################################################
 
