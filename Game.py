e667b182340feb0dbc4e6e72e8de6b529dd7af9f
Tet Nay Lin <tetnaylin168@gmail.com>
2025-07-24 16:41:45 +1000

    Symbol in debug statement

--- Game.py
+++ Game.py
@@ -84,7 +84,7 @@ class Pickup_State(Game_State):
         else:
             self.game.change_state(Error_State(self.colour, self.game, self))
             self.game.error_lightup()
-            print(f"Panik_DOWN: {self.colour}")
+            print(f"Panik_DOWN: {self.colour}, Symbol: {self.game.chessboard.piece_at(chess.parse_square(changes[0]['square'])).symbol()}")
         
 
 class Putdown_State(Game_State):
@@ -111,7 +111,7 @@ class Putdown_State(Game_State):
         else:
             self.game.change_state(Error_State(self.colour, self.game, self))
             self.game.error_lightup()
-            print(f"Panik_UP: {self.colour}")
+            print(f"Panik_UP: {self.colour}, Symbol: {self.game.chessboard.piece_at(chess.parse_square(changes[0]['square'])).symbol()}")
     
     def finalise_move(self, to_square):
             self.game.chessboard.push(chess.Move(self.game.from_square, to_square))
