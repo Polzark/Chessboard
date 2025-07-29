e802edaadf2255762099aa993c9b10a45b705b87
Tet Nay Lin <tetnaylin168@gmail.com>
2025-07-29 19:17:43 +1000

    Add led test

---
+++ test_led.py
@@ -0,0 +1,12 @@
+import neopixel
+import board
+import time
+
+pixels = neopixel.NeoPixel(board.D18, 8, auto_write=False)
+pixels.fill((255,0,0))
+pixels.show()
+print("Sleeping for 3s now board should be read hopefully\n")
+time.sleep(3)
+pixels.fill((0,0,0))
+pixels.show()
+print("Now, I die lol\n")
