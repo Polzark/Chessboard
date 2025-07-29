ba0af2f63aaf9ecedb669e357040cbfe4b2b124e
Tet Nay Lin <tetnaylin168@gmail.com>
2025-07-29 19:22:40 +1000

    Test with diff neopixel

--- test_led.py
+++ test_led.py
@@ -3,10 +3,10 @@ import board
 import time
 
 pixels = neopixel.NeoPixel(board.D18, 8, auto_write=False)
-pixels.fill((255,0,0))
+pixels.fill((255,0,0,0))
 pixels.show()
 print("Sleeping for 3s now board should be read hopefully\n")
 time.sleep(3)
-pixels.fill((0,0,0))
+pixels.fill((0,0,0,0))
 pixels.show()
 print("Now, I die lol\n")
