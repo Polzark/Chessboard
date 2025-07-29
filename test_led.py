9c1eeb7b33a27614a5ceb9cf3d135eed46db1f8f
Tet Nay Lin <tetnaylin168@gmail.com>
2025-07-29 19:27:12 +1000

    Add try except to test

--- test_led.py
+++ test_led.py
@@ -2,11 +2,14 @@ import neopixel
 import board
 import time
 
-pixels = neopixel.NeoPixel(board.D18, 8, auto_write=False)
-pixels.fill((255,0,0))
-pixels.show()
-print("Sleeping for 3s now board should be red hopefully\n")
-time.sleep(3)
-pixels.fill((0,0,0))
-pixels.show()
-print("Now, I die lol\n")
+try:
+    pixels = neopixel.NeoPixel(board.D18, 8, auto_write=False)
+    pixels.fill((255,0,0))
+    pixels.show()
+    print("Sleeping for 3s now board should be red hopefully\n")
+    time.sleep(3)
+    pixels.fill((0,0,0))
+    pixels.show()
+    print("Now, I die lol\n")
+except:
+    print("lol")
