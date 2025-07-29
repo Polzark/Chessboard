33cade727003828489638e35a2c2e3068a16eb94
Tet Nay Lin <tetnaylin168@gmail.com>
2025-07-29 19:35:43 +1000

    ahh

--- Game.py
+++ Game.py
@@ -32,8 +32,7 @@ class Game:
             ahh = chess.parse_square(square)
             row = chess.square_file(ahh)
             col = chess.square_rank(ahh)
-            if (col == 0):
-                self.pixels[row] = (255,0,0)
+            self.pixels[row][col] = (255,0,0)
         print(self.pixels)
         self.pixels.show()
     
